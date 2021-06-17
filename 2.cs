68b3942ca8cc94f37192ec4d9659f1b122770ffe
sergiosantg <86072961+sergiosantg@users.noreply.github.com>
2021-06-17 19:24:02 +0200

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine('Hola mundo!!');
+            int num1 = 5;
+            int numero2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El número introducido es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+        }
+    }
+
+}
